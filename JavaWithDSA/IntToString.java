7dc43dee829dfc2671c2149e9f52bdb489a00c94
Subodhkumar <snp42u@gmail.com>
2023-03-07 23:43:57 +0530

    Update

--- JavaWithDSA/IntToString.java
+++
@@ -1,8 +0,0 @@
-
-public class IntToString{  
-    public static void main(String args[]){  
-    int i=200;  
-    String s=String.valueOf(i);  
-    System.out.println(i+100);
-    System.out.println(s+100);
-    }}  
--- JavaWithDSA/str.java
+++
@@ -1,8 +0,0 @@
-public class str {
-    public static void main(String[] args) {
-        
-    String st = "Subodh";
-    String st2 = st + " Kumar";
-    System.out.println(st2);
-    }
-}
